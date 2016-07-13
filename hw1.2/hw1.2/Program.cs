7372e203cf10e4f4c5e1b7d631e197ae06278ea8
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-13 19:36:39 +0300

    fixed hw 1.2

--- hw1.2/hw1.2/Program.cs
+++ hw1.2/hw1.2/Program.cs
@@ -26,6 +26,11 @@ namespace hw1._2
             string hex3 = String.Format("{0:X}", dec3);
             Console.WriteLine("Char {0}: dec {1} hex {2}", letter3, dec3, hex3);
 
+            char l = 'l';
+            int d = Convert.ToInt32(l);
+            string h = String.Format("{0:X}", d);
+            Console.WriteLine("Char {0}: dec {1} hex {2}", l, d, h);
+
             char letter4 = 'o';
             int dec4 = Convert.ToInt32(letter4);
             string hex4 = String.Format("{0:X}", dec4);
@@ -36,10 +41,20 @@ namespace hw1._2
             string hex5 = String.Format("{0:X}", dec5);
             Console.WriteLine("Char {0}: dec {1} hex {2}", letter5, dec5, hex5);
 
+            char a = 'o';
+            int b = Convert.ToInt32(a);
+            string c = String.Format("{0:X}", b);
+            Console.WriteLine("Char {0}: dec {1} hex {2}", a, b, c);
+
             char letter6 = 'r';
             int dec6 = Convert.ToInt32(letter6);
             string hex6 = String.Format("{0:X}", dec6);
             Console.WriteLine("Char {0}: dec {1} hex {2}", letter6, dec6, hex6);
+
+            char l1 = 'l';
+            int d1 = Convert.ToInt32(l1);
+            string h1 = String.Format("{0:X}", d1);
+            Console.WriteLine("Char {0}: dec {1} hex {2}", l1, d1, h1);
 
             char letter7 = 'd';
             int dec7 = Convert.ToInt32(letter7);
