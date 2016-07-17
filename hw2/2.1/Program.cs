51b172472598c08e89d5083005157d2c4ba05b7f
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-17 12:34:39 +0300

    hw 2.1 added

---
+++ hw2/2.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 != 0)
+                {
+                    arr[i] = (arr[i] + 1) % 10;
+                }
+                Console.Write("{0} ", arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
