137391604dbca9371784b74b044a6705199d7906
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-17 13:52:13 +0300

    hw 2.3 added

--- hw2/2.3/Program.cs
+++ hw2/2.3/Program.cs
@@ -10,7 +10,25 @@ namespace _2._3
     {
         static void Main(string[] args)
         {
-            
+            int x = 5;
+            int y = 12;
+            int z = 2;
+
+//Рассчитать по формуле:
+            //1)x++ = 6;
+            //2)z++ = 3;
+            //3)--y = 11;
+            //4)--x = 5;
+            //5)x++ - --y + --x = -1; далее мы -1 сдвигаем влево на z++ = 3 позиции; 
+            //6)x++ - --y + --x << z++ = -4;
+            //7)x + y + z = 19;
+            //8)(x++ - --y + --x << z++)*(x + y + z) = -4 * 19 = -76;
+           
+            int result = (x++ - --y + --x << z++)*(x + y + z);
+
+
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
