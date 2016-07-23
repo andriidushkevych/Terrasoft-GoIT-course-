569252f52fc63959c5f51457c232e48fce60a67b
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-23 13:56:37 +0300

    hw 2.1 fixed

--- hw2/2.1/Program.cs
+++ hw2/2.1/Program.cs
@@ -12,26 +12,23 @@ namespace _2._1
         {
 
             int num = 1234567890;
-            int[] arr = new int[10];
             string strNum = Convert.ToString(num);
 
             Console.WriteLine(strNum);//to check wtf is in this string. its ok
 
-            for (int i = 0; i < strNum.Length; i++)
-            {
-                arr[i] = Convert.ToInt32(strNum[i]);//something is going wrong here, cant understand what exactly.
-                Console.Write(arr[i] + " ");// why string "1234567890" being char by char written to int array, appears as "49 50 51 52 53 54 55 56 57 48???????????????????"
-            }
+           
 
             Console.WriteLine(" ");
 
-                for (int i = 0; i < arr.Length; i++)
+                for (int i = 0; i < strNum.Length; i++)
                 {
-                    if (arr[i] % 2 != 0)
+                    if (strNum[i] % 2 != 0)
                     {
-                        arr[i] = (arr[i] + 1) % 10;
+                        int a = (int)Char.GetNumericValue(strNum[i]);
+                        a = (a + 1) % 10;
+                        Console.Write("{0} ", a);
                     }
-                    Console.Write("{0} ", arr[i]);
+                    else Console.Write("{0} ", strNum[i]);
                 }
 
             Console.ReadKey();
