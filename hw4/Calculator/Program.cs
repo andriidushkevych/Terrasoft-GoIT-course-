fe4f3a2120e9eea72732cccd3752fe77805c793c
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-24 14:59:32 +0300

    error messages in hw4 added

--- hw4/Calculator/Program.cs
+++ hw4/Calculator/Program.cs
@@ -11,32 +11,32 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.Write("Input first digit: ");
-            Console.WriteLine(" ");
             string first = Console.ReadLine();
+            Console.WriteLine(" ");
             double a = Convert.ToDouble(first);
 
-            while (true)
+            for (int i = 0; i < 1000; i++ )
             {
 
                 string oper = GetOperation();
                 double b = GetAnotherDigit();
 
                 double res = Calculate(a, b, oper);
-
+                
                 Console.WriteLine("result = {0}", res);
                 Console.WriteLine(" ");
 
                 a = res;
 
             }
+            Console.ReadKey();
         }
 
         static string GetOperation()
         {
-            Console.WriteLine("Choose operation:\n 1: '+' \n 2: '-' \n 3: '/' \n 4: '*' \n 5: '%' \n 6: 'pow' \n");
+            Console.Write("Choose operation:\n 1: '+' \n 2: '-' \n 3: '/' \n 4: '*' \n 5: '%' \n 6: 'pow' \n");
             Console.WriteLine(" ");
-            string res = Console.ReadLine();
-            
+            string res = Console.ReadLine();            
             return res;
         }
 
@@ -45,16 +45,25 @@ namespace Calculator
             Console.Write("Input another digit: ");
             Console.WriteLine(" ");
             string y = Console.ReadLine();
-            double b = Convert.ToDouble(y);
+            double b = 0;
+            if (!double.TryParse(y, out b))
+            {
+                Console.WriteLine("You had to input a digit!!!");
+            }else  b = Convert.ToDouble(y);
+            
             return b;
         }
 
         static double Calculate(double aArg, double bArg, string operArg)
         {
-            int oper = Convert.ToInt32(operArg);
+            int oper;
             double result = 0;
+            if (operArg == "+") { oper = 1; } else if (operArg == "-") { oper = 2; } else if (operArg == "/") { oper = 3; } 
+            else if (operArg == "*") { oper = 4; } else if (operArg == "%") { oper = 5; } else if (operArg == "pow") { oper = 6; }
+            else { oper = 0; Console.WriteLine("You had to input one of those operations (+ - / * % pow)!"); }
+
             switch(oper)
-            {
+            {                
                 case (1):
                     {
                         result = Addition(aArg, bArg);
@@ -106,6 +115,11 @@ namespace Calculator
 
         static double Division(double xArg, double yArg)
         {
+            if (yArg == 0)
+            {
+                Console.WriteLine("Forbidden operation");
+            }
+
             double result = xArg / yArg;
 
             return result;
