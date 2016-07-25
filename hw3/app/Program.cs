b5d23ec72a7008e0e8bd07bb63ac8ad8c2fa4528
Andrii Dushkevych <andrey.dushkevich@gmail.com>
2016-07-25 15:52:10 +0300

    hw3 fixed

--- hw3/app/Program.cs
+++ hw3/app/Program.cs
@@ -10,41 +10,56 @@ namespace app
     {
         static void Main(string[] args)
         {
-            string[] logins = new string[] { "root", "moder", "aaa"};
-            string[] passwords = new string[] { "qwe", "123", "qwerty" };
-            string[] roles = new string[] { "admin", "moderator", "user" };
-            
-            Console.WriteLine("Input your login: ");
+
+            int countOfUsers = 0;
+            string nameOfUsers = "";
+
+            string[] roles = { "Admin", "Moderator", "Moderator", "User", "User", "User" };
+            string[] logins = { "admin", "moder1", "moder2", "user1", "user1", "user1" };
+            string[] passwords = { "a1", "m1", "m2", "u1", "u2", "u3" };
+
+            Console.Write("Please, enter your login:");
             string log = Console.ReadLine();
 
-            Console.WriteLine("Input your password: ");
-            string pass = Console.ReadLine();
+            int attempts = 3;
 
-            if (log == logins[0] && pass == passwords[0])
+            for (int i = 0; i < logins.Length; i++)
             {
-                Console.WriteLine("Hello, Admin! \n");
-                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[0], passwords[0], roles[0]);
-                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[1], passwords[1], roles[1]);
-                Console.WriteLine("User: {0}. Password: {1}. Role: {2}.", logins[2], passwords[2], roles[2]);
-            }
-            else if (log == logins[1] && pass == passwords[1])
-            {
-                Console.WriteLine("Hello, Moderator! \n");
-                Console.WriteLine("There are 3 users in our system");
-                
-            }
-            else if (log == logins[2] && pass == passwords[2])
-            {
-                Console.WriteLine("Hello, User! \n");
-                Console.WriteLine("User with role user: {0}", logins[2]);
-                
-            }
-            else
-            {
-                Console.WriteLine("Your data is incorrect");
-            }
+                if (log != logins[i])  continue; 
 
+                for (int j = 0; j < attempts; j++)
+                {
+                    Console.Write("Please, enter your password:");
+                    var pass = Console.ReadLine();
 
+                    if (pass == passwords[i])
+                    {
+                        switch (roles[i])
+                        {
+                            case "Admin":
+                                for (int k = 0; k < logins.Length; k++)
+                                    Console.WriteLine("{0}: Login: {1}, Password: {2}", roles[k], logins[k], passwords[k]);
+                                break;
+                            case "Moderator":
+                                Console.WriteLine("Quantity of all users: {0}", logins.Length);
+                                break;
+                            default:
+                                for (int l = 0; l < roles.Length; l++)
+                                    if (roles[l] == "User")
+                                    {
+                                        countOfUsers++;
+                                        nameOfUsers += logins[l] + "; ";
+                                    }
+
+                                Console.WriteLine("Quantity with role users {0}-users. User names: {1}", countOfUsers, nameOfUsers);
+                                break;
+                        }
+                        break;
+                    }
+                    Console.WriteLine("Password is incorrect. You have {0} more attempts left!", (2-j));
+                }
+            }
+            
             Console.ReadKey();
         }
     }
